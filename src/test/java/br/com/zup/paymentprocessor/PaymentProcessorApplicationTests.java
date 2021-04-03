e7317b93959cf6cafaa781caaa70e30e140c82bd
Marcos Vidolin <marcosvidolin@gmail.com>
2021-04-03 19:01:38 -0300

    chore: atributos

--- src/test/java/br/com/zup/paymentprocessor/PaymentProcessorApplicationTests.java
+++ src/test/java/br/com/zup/paymentprocessor/PaymentProcessorApplicationTests.java
@@ -1,8 +1,5 @@
 package br.com.zup.paymentprocessor;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
 //@SpringBootTest
 class PaymentProcessorApplicationTests {
 
